6828556d955c51e2b2f4d858b493b51ca3000f52
Zachary DuBow <zachary.dubow@gmail.com>
2021-07-06 18:03:44 +0300

    form

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -5,23 +5,25 @@ export default function Contact() {
   const [email, setEmail] = useState("");
   const [message, setMessage] = useState("");
 
-  function encode(data) {
-    return Object.keys(data)
-      .map(
-        (key) => encodeURIComponent(key) + "=" + encodeURIComponent(data[key])
-      )
-      .join("&");
-  }
+  //function encode(data) {
+  //  return Object.keys(data)
+  //    .map(
+  //      (key) => encodeURIComponent(key) + "=" + encodeURIComponent(data[key])
+  //    )
+  //    .join("&");
+  //}
 
   function handleSubmit(e) {
     e.preventDefault();
-    fetch("/", {
-      method: "POST",
-      headers: { "Content-Type": "application/x-www-form-urlencoded" },
-      body: encode({ "form-name": "contact", name, email, message }),
-    })
-      .then(() => alert("Message sent!"))
-      .catch((error) => alert(error));
+    alert("Message sent!");
+
+    //fetch("/", {
+    //  method: "POST",
+    //  headers: { "Content-Type": "application/x-www-form-urlencoded" },
+    //  body: encode({ "form-name": "contact", name, email, message }),
+    //})
+    //  .then(() => )
+    //  .catch((error) => alert(error));
     setName("");
     setEmail("");
     setMessage("");
