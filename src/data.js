63fa9804db556bf4fa75509d5ceb1beff133f586
Zachary DuBow <zachary.dubow@gmail.com>
2021-07-06 11:21:52 +0300

    first commit

---
+++ src/data.js
@@ -0,0 +1,84 @@
+export const projects = [
+  {
+    title: "Restaurant",
+    subtitle: "React Front-end + Firebase",
+    description:
+      "Food ordering app built in React that demonstrates fundamental React tooling like React-Router, Context, Lazy Loading optimization, and Firebase backend.",
+    image: "./restaurant.gif",
+    link: "www.google.com",
+  },
+  {
+    title: "FertiChat",
+    subtitle: "React and ExpressJS",
+    description:
+      "Medical e-commerce site specialing in male/female fertility selling testing solutions and coaching consultations. APIs with Stripe, Calendly, and 3rd party testing vendors.",
+    image: "./fertichat.gif",
+    link: "https://fertichat-wip.herokuapp.com",
+  },
+  {
+    title: "Contact Manager & ToDos Lists",
+    subtitle: "ExpressJS",
+    description:
+      "RESTful API with combined contact manager and todo lists using express-session module for data persistence. Pug templating and Flash messaging.",
+    image: "./assistant.gif",
+    link: "https://pa-app-zd.herokuapp.com",
+  },
+  {
+    title: "GUI Apps",
+    subtitle: "Vanilla JS, HTML, CSS",
+    description:
+      "A collection of various small(er) apps built with vanilla HTML, CSS, and JS application logic.",
+    image: "./gui-apps.gif",
+    link: "https://github.com/zacharydub/gui_apps",
+  },
+];
+
+export const testimonials = [
+  {
+    quote:
+      "As Zach’s manager at Freightos I worked closely with him for three years. Zach joined as an associate but quickly became an expert in the field and took on team lead and then management responsibilities. As the business grew Zach took over two critical teams - our freight operations team and our customer success team. Zach’s teams were spread over 4 countries and provided round-the-clock coverage. He was very comfortable with our complicated tech stack and built training that allowed for quick onboarding of new team members. The proof in the pudding is that under Zach’s management we were able to triple our bookings and increase our customer satisfaction to a steady NPS of 60+ and “Excellent” rating on TrustPilot. I am confident that Zach will contribute significantly to any team he joins!",
+    name: "Ruthie Amaru",
+    company: "Freightos",
+    profile: "https://www.linkedin.com/in/ruthieamaru/",
+    role: "CEO",
+  },
+  {
+    quote:
+      "I had the honor to work with Zach since the first day he joined the company.Zach is a true hard worker and a very dedicated team member. We all knew that if Zach is working on a project it will be done the best way we can imagine. Zach took on himself so many projects and did it in the best way one can do. Zach will definitely be a huge asset to any team he joins",
+    name: "Avishai Zussman",
+    company: "Freightos",
+    profile: "https://www.linkedin.com/in/avishai-zussman-826333106/",
+    role: "Product Manager",
+  },
+  {
+    quote:
+      "During the time I worked with Zack I enjoyed watching him excel in his role in customer success and operations. The dedication and commitment he brought with him to the job every day was inspiring. The team of CSMs he built also exemplified his strong work ethic. He was a pleasure to be around and I know he will bring the same passion anywhere he goes.",
+    name: "Noah Alhadeff",
+    company: "Sisense",
+    profile: "https://www.linkedin.com/in/noahalhadeff/",
+    role: "Sales Manager",
+  },
+  {
+    quote:
+      "It has been excellent working together with Zach and I have a lot of respect for his skills. Intelligent, sharp and well educated Ops Manager who could always be trusted. Zach is dedicated, hard working, methodical, and very capable. He was always able to resolve difficult problems under pressure and find a way to weather any storm, even proposing new and fresh ideas. Zach is an asset to any team that wants to get things done.",
+    name: "Eze Fiszerman",
+    company: "Freightos",
+    profile: "https://www.linkedin.com/in/eze-fiszerman/",
+    role: "Head of Operations",
+  },
+];
+
+export const skills = [
+  "JavaScript",
+  "React",
+  "Angular",
+  "HTML",
+  "CSS",
+  "NodeJS",
+  "ExpressJS",
+  "Bootstrap / Tailwind",
+  "PostgreSQL",
+  "MongoDB",
+  "Jest",
+  "TypeScript",
+];
