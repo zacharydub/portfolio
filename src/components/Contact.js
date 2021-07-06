8d0b1d90fdf08a8a717c1bd65df33c600d68cf8a
Zachary DuBow <zachary.dubow@gmail.com>
2021-07-06 18:15:13 +0300

    form

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -74,7 +74,7 @@ export default function Contact() {
           action="https://formsubmit.co/el/newabo"
           method="POST"
           onSubmit={handleSubmit}
-          //name="contact"
+          name="contact"
           className="lg:w-full md:w-1/2 flex flex-col md:ml-auto w-full md:py-8 mt-8 md:mt-0"
         >
           <h2 className="text-white sm:text-4xl text-3xl mb-1 font-medium title-font">
