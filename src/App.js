17ff6dee8fe2db495d9f852dfcaba9f09fcefb1d
Zachary DuBow <zachary.dubow@gmail.com>
2021-07-15 16:32:42 +0300

    matrix

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Navbar from "./components/Navbar";
 import Projects from "./components/Projects";
 import Skills from "./components/Skills";
 import Testimonials from "./components/Testimonials";
-
+import Footer from "./components/Footer";
 export default function App() {
   return (
     <main className="text-gray-400 bg-gray-900 body-font">
@@ -16,6 +16,7 @@ export default function App() {
       <Testimonials />
       <Skills />
       <Contact />
+      <Footer />
     </main>
   );
 }
---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Footer() {
+  return (
+    <div id="matrix">
+      <img src="matrix-1s0s.jpg" width="90%" />
+      {/*<img src="matrix-foreign.jpg" width="500px" />*/}
+      {/*<img src="matrix-neon.jpg" width="500px" />*/}
+    </div>
+  );
+}
