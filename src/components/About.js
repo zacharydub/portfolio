16c51b3783210f3c4ac767d77126933558910557
Zachary DuBow <zachary.dubow@gmail.com>
2022-05-12 13:46:59 +0300

    about

--- src/components/About.js
+++ src/components/About.js
@@ -25,7 +25,7 @@ export default function About() {
               href="#projects"
               className="ml-4 inline-flex text-gray-400 bg-gray-800 border-0 py-2 px-6 focus:outline-none hover:bg-gray-700 hover:text-white rounded text-lg"
             >
-              See My Past Work
+              Some Apps
             </a>
           </div>
         </div>
--- src/components/Skills.js
+++ src/components/Skills.js
@@ -9,13 +9,13 @@ export default function Skills() {
         <div className="text-center mb-20">
           <ChipIcon className="w-10 inline-block mb-4" />
           <h1 className="sm:text-4xl text-3xl font-medium title-font text-white mb-4">
-            Skills &amp; Technologies
+           Tech Stack
           </h1>
           <p className="text-base leading-relaxed xl:w-2/4 lg:w-3/4 mx-auto">
-            I am a full stack web developer and I love learning new things
+            {/*I am a full stack web developer and I love learning new things
             daily. Currently I favor JavaScript and its frontend frameworks
             React and Angular. I use NodeJS/ExpressJS for the backend,
-            PostgreSQL and MongoDB for data.
+            PostgreSQL and MongoDB for data.*/}
           </p>
         </div>
         <div className="flex flex-wrap lg:w-4/5 sm:mx-auto sm:mb-2 -mx-2">
